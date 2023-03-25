9070cbc7628257d96d286d53010c2e47a6d9fdfe
Justynasd <justynasd@gmail.com>
2023-03-25 23:02:20 +0200

    init nuxt3,@nuxtjs/i18n,vuetify3

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    locales: [
+      {
+        code: "lt",
+        name: "Lietuvių",
+        iso: "lt-LT",
+        files: ["lt-LT.json"],
+      },
+      {
+        code: "en",
+        name: "English",
+        iso: "en-US",
+        files: ["en-US.json"],
+      },
+    ],
+    lazy: true,
+    strategy: "prefix",
+    langDir: "lang",
+    defaultLocale: "lt",
+    vueI18n: {
+      // If fallback is needed, you need to define
+      fallbackLocale: "lt",
+    },
+  },
+});
